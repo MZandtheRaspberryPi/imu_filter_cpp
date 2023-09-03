4d8f3fecd4965d13dfa6f886da3fac3b64b848a3
mzandtheraspberrypi@gmail.com <mzandtheraspberrypi@gmail.com>
2023-09-03 16:15:27 +0200

    adding publishing of filter message

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM ubuntu:20.04
+FROM ubuntu:20.04 as build
 
 RUN apt-get update
 ENV TZ=Europe/London
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-RUN  apt-get install nano git cmake build-essential g++ libeigen3-dev -y
+RUN  apt-get install nano git cmake build-essential g++ libeigen3-dev protobuf-compiler libboost-all-dev -y
 
 COPY . /repo
 
@@ -13,3 +13,11 @@ RUN mkdir build
 WORKDIR /repo/build
 RUN cmake ..
 RUN make
+
+FROM ubuntu:22.04
+
+COPY --from=build /repo/build/imu_filter_cpp_main /entrypoint/imu_filter_cpp_main
+
+STOPSIGNAL SIGINT
+
+ENTRYPOINT ["/entrypoint/imu_filter_cpp_main"]
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,31 @@
 #include "filter_saito_ekf.h"
+#include "util.h"
+#include "websocket_broadcaster.h"
+#include "websocket_listener.h"
 
-int main() {
+bool EXIT_FLAG = false;
+const uint16_t LOOP_DELAY_MS = 10;
+
+void my_signal_handler(int s) {
+  printf("Caught signal %d\n", s);
+  EXIT_FLAG = true;
+}
+
+void setup_sigint_handler(struct sigaction &sig_int_handler) {
+  sig_int_handler.sa_handler = my_signal_handler;
+  sigemptyset(&sig_int_handler.sa_mask);
+  sig_int_handler.sa_flags = 0;
+  sigaction(SIGINT, &sig_int_handler, NULL);
+}
+
+int main(int argc, char *argv[]) {
+  // Verify that the version of the library that we linked against is
+  // compatible with the version of the headers we compiled against.
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  struct sigaction sig_int_handler;
+  setup_sigint_handler(sig_int_handler);
+
   SaitoIMUSystemModel::StateCovarianceMatrix Q;
   Q.setIdentity();
 
@@ -16,4 +41,81 @@ int main() {
   sensor_to_base.setIdentity();
 
   EKFSaitoModel ekf_saito(sensor_to_base, Q, R);
+
+  ListenerClient listener_server = ListenerClient();
+  std::string ip_address = "broadcaster";
+  uint16_t port = 9000;
+  listener_server.connect(ip_address, port);
+  listener_server.start_non_blocking();
+
+  BroadcastServer broadcast_server = BroadcastServer();
+  broadcast_server.start_listening(9001);
+
+  uint64_t last_timestamp = 0;
+
+  while (!EXIT_FLAG) {
+    if (listener_server.has_msg()) {
+      imu_msgs::ImuMsg msg = listener_server.get_msg();
+      if (!msg.IsInitialized()) {
+        continue;
+      }
+
+      uint64_t cur_timestamp = msg.timestamp();
+
+      if (last_timestamp == 0) {
+        last_timestamp = cur_timestamp;
+        continue;
+      }
+
+      m_t delta_t = (cur_timestamp - last_timestamp) / 1000;
+
+      SaitoIMUSystemModel::SensorDataMatrix angular_accel{
+          msg.angular_acceleration().x(), msg.angular_acceleration().y(),
+          msg.angular_acceleration().z()};
+
+      EKFSaitoModel::EstimateAndCovariance estimate_given_t_minus_one =
+          ekf_saito.predict(estimate_and_cov, angular_accel, delta_t);
+
+      SaitoIMUSystemModel::SensorDataMatrix accelerometer{
+          msg.linear_acceleration().x(), msg.linear_acceleration().y(),
+          msg.linear_acceleration().z()};
+
+      SaitoIMUSystemModel::SensorDataMatrix magnetometer{
+          msg.magnetometer_vector().x(), msg.magnetometer_vector().y(),
+          msg.magnetometer_vector().z()};
+
+      EKFSaitoModel::EstimateAndCovariance estimate_given_t =
+          ekf_saito.update(estimate_given_t_minus_one, accelerometer,
+                           angular_accel, magnetometer, delta_t);
+
+      estimate_and_cov = estimate_given_t;
+
+      imu_msgs::FilterMsg filter_msg;
+      filter_msg.set_timestamp(get_timestamp());
+      imu_msgs::ImuMsg *imu_msg_filter = filter_msg.mutable_imu_msg();
+
+      imu_msg_filter->CopyFrom(msg);
+
+      imu_msgs::Triad *euler_angles = filter_msg.mutable_euler_angles_filter();
+      euler_angles->set_x(estimate_and_cov.state_estimate(0, 0));
+      euler_angles->set_y(estimate_and_cov.state_estimate(1, 0));
+      euler_angles->set_z(estimate_and_cov.state_estimate(2, 0));
+
+      size_t msg_size = filter_msg.ByteSizeLong();
+      uint8_t *msg_arr = new uint8_t[msg_size];
+      filter_msg.SerializeToArray(msg_arr, msg_size);
+      broadcast_server.send_message(msg_arr, msg_size);
+      delete[] msg_arr;
+
+      std::string debug_str = filter_msg.DebugString();
+      std::cout << debug_str << std::endl;
+    }
+
+    delay(LOOP_DELAY_MS);
+  }
+
+  listener_server.stop();
+  broadcast_server.stop_listening();
+
+  return 0;
 }
